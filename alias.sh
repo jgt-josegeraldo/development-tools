2f01b69523f2289974d431574de7e26e1c05f318
jgt-josegeraldo <jgt.josegeraldo@gmail.com>
2018-03-30 14:26:05 -0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,7 +1,16 @@
-# php 56
+#
 alias composer="sudo docker run --rm -u $UID -v $PWD:/app composer/composer"
 
 
+# php 56
+alias php56="sudo docker run --rm -u $UID -v $PWD:/app php:5.6-fpm php"
+
+# php 7
+alias php7="sudo docker run --rm -u $UID -v $PWD:/app php:7 php"
+
+
+# php 7.2
+alias php72="sudo docker run --rm -u $UID -v $PWD:/app php:7.2 php"
 
 #Fron-end
 alias node="sudo docker run -ti --rm -u $UID -v $PWD:/data dragonmantank/nodejs-grunt-bower node"
