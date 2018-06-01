a053b80b7715446d38a902e6fcfdd19eb513b693
José Geraldo da Costa <jgt.josegeraldo@gmail.com>
2018-06-01 10:57:56 -0300

    .

--- alias.sh
+++ alias.sh
@@ -1,21 +1,22 @@
-#
-alias composer="sudo docker run --rm -u $UID -v $PWD:/app composer/composer"
+# phpunit
+alias phpunit56="sudo docker run --rm -u $UID -v \$PWD:/app -w /app --name phpunit56 rubeus/phpunit5.6 phpunit"
+alias phpunit7="sudo docker run --rm -u $UID -v \$PWD:/app  -w /app --name phpunit7 rubeus/phpunit7 phpunit"
+alias phpunit72="sudo docker run --rm -u $UID -v \$PWD:/app  -w /app --name phpunit72 rubeus/phpunit7.2 phpunit"
+
+# composer
+alias composer56="sudo docker run --rm -u $UID -v \$PWD:/app  -w /app --name composer56 rubeus/composer5.6 composer"
+alias composer7="sudo docker run --rm -u $UID -v \$PWD:/app  -w /app --name composer7 rubeus/composer7 composer"
+alias composer72="sudo docker run --rm -u $UID -v \$PWD:/app  -w /app --name composer72 rubeus/composer7.2 composer"
 
 
 # php 56
-alias php56="sudo docker run --rm -u $UID -v $PWD:/app php:5.6-fpm php"
-
-# php 7
-alias php7="sudo docker run --rm -u $UID -v $PWD:/app php:7 php"
-
-
-# php 7.2
-alias php72="sudo docker run --rm -u $UID -v $PWD:/app php:7.2 php"
+alias php56="sudo docker run --rm -u $UID -v \$PWD:/app  -w /app --name php56 rubeus/php5.6 php"
+alias php7="sudo docker run --rm -u $UID -v \$PWD:/app  -w /app --name php7 rubeus/php7 php"
+alias php72="sudo docker run --rm -u $UID -v \$PWD:/app  -w /app --name php72 rubeus/php7.2 php"
 
 #Fron-end
-alias node="sudo docker run -ti --rm -u $UID -v $PWD:/data dragonmantank/nodejs-grunt-bower node"
-alias grunt="sudo docker run -ti --rm -u $UID -v $PWD:/data dragonmantank/nodejs-grunt-bower grunt"
-alias npm="sudo docker run -ti --rm -u $UID -v $PWD:/data dragonmantank/nodejs-grunt-bower npm"
-alias bower="sudo docker run -ti --rm -u $UID -v $PWD:/data dragonmantank/nodejs-grunt-bower bower"
-alias sass="sudo docker run -ti --rm -u $UID -v $PWD:/data jbergknoff/sass sass"
-
+alias node="sudo docker run --rm -u $UID -v \$PWD:/app -v ~/node_modules:/usr/local/lib/node_modules -w /app --name node rubeus/node-npm node"
+alias grunt="sudo docker run --rm -u $UID -v \$PWD:/app -v ~/node_modules:/usr/local/lib/node_modules -w /app --name grunt rubeus/grunt grunt"
+alias npm="sudo docker run --rm -u $UID -v \$PWD:/app -v ~/node_modules:/usr/local/lib/node_modules -w /app --name npm rubeus/node-npm npm"
+alias sass="sudo docker run --rm  -u $UID  -v \$PWD:/app -v ~/node_modules:/usr/local/lib/node_modules -w /app --name sass rubeus/sass sass"
+alias ng="sudo docker run --rm  -u $UID -v \$PWD:/app -v ~/node_modules:/usr/local/lib/node_modules -w /app --name ng -p 4200:4200 rubeus/angular.io ng "
---
+++ buildImages.sh
@@ -0,0 +1,38 @@
+cd node-npm/
+docker build -t rubeus/node-npm .
+
+cd ../php/php7/
+docker build -t rubeus/php7 .
+
+cd ../php5/
+docker build -t rubeus/php5.6 .
+
+cd ../php7.2/
+docker build -t rubeus/php7.2 .
+
+cd ../../angular/angular.io/
+docker build -t rubeus/angular.io .
+
+cd ../../composer/composer5/
+docker build -t rubeus/composer5.6 .
+
+cd ../composer7.2/
+docker build -t rubeus/composer7.2 .
+
+cd ../composer7/
+docker build -t rubeus/composer7 .
+
+cd ../../grunt/
+docker build --no-cache -t rubeus/grunt .
+
+cd ../phpunit/phpunit7/
+docker build -t rubeus/phpunit7 .
+
+cd ../phpunit7.2/
+docker build -t rubeus/phpunit7.2 .
+
+cd ../phpunit5/
+docker build -t rubeus/phpunit5.6 .
+
+cd ../../sass/
+docker build --no-cache -t rubeus/sass .
